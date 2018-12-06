2041dfb5872fe705ca2d3b8bb52ed2ae89c1e30b
Caro <carolinadiaz017@gmail.com>
2018-12-06 01:12:26 -0500

    responsive

--- js/changing_bars.js
+++ js/changing_bars.js
@@ -5,6 +5,32 @@
     var selected;
 
 
+                 function responsivefy(svg) {
+    // get container + svg aspect ratio
+    var container = d3.select(svg.node().parentNode),
+        width = parseInt(svg.style("width")),
+        height = parseInt(svg.style("height")),
+        aspect = width / height;
+
+    // add viewBox and preserveAspectRatio properties,
+    // and call resize so that svg resizes on inital page load
+    svg.attr("viewBox", "0 0 " + width + " " + height)
+        .attr("perserveAspectRatio", "xMinYMid")
+        .call(resize);
+
+    // to register multiple listeners for same event type, 
+    // you need to add namespace, i.e., 'click.foo'
+    // necessary if you call invoke this function for multiple svgs
+    // api docs: https://github.com/mbostock/d3/wiki/Selections#on
+    d3.select(window).on("resize." + container.attr("id"), resize);
+
+    // get width of container and resize svg to fit it
+    function resize() {
+        var targetWidth = parseInt(container.style("width"));
+        svg.attr("width", targetWidth);
+        svg.attr("height", Math.round(targetWidth / aspect));
+    }
+}
         //setup the svg
 
         var vis = d3.select("#changing_bars").append("svg");
@@ -14,12 +40,16 @@
         svg.append("rect")
             .attr("width", "100%")
             .attr("height", "100%")
-            .attr("fill", "none");
-    
+            .attr("fill", "none")
+        .call(responsivefy);
 
+    
+           
 
 
         d3.csv("data/yearlydiets.csv", function(error, data) {
+            
+
 
           var selected = 2004;
           //console.log(column);
